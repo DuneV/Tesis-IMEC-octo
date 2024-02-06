0e572e2d7666ca0142372acb30cf1bed5c97fd62
Daniel Alejandro Alvarez Vanegas <96156811+DuneV@users.noreply.github.com>
2024-02-06 00:18:55 -0500

    update

--- cinema.py
+++ cinema.py
@@ -8,7 +8,8 @@ import numpy as np
 
 # se define una sección como una parte del brazo
 class Seccion:
-    
+    # metodo constructor para varibles de tipo globales
+
     def __init__(self, N_prev, O_prev, q1_val, q2_val, q3_val, d_A_val, d_B_val, delta_x_val, name_suffix):
         self.q1, self.q2, self.q3 = dynamicsymbols(f'q1_{name_suffix} q2_{name_suffix} q3_{name_suffix}')
         self.d_A, self.d_B, self.delta_x = symbols(f'd_A_{name_suffix} d_B_{name_suffix} delta_x_{name_suffix}')
@@ -22,22 +23,36 @@ class Seccion:
             self.delta_x: delta_x_val
         }
         
-        self.angle = 2 * pi / 3
-        self.angle2 = 4 * pi / 3
-
+        self.angle = 2 * pi / 3 # dispersión de resorte 2
+        self.angle2 = self.angle * 2 # dispersión de resorte 3
+        
+        # marco de referencia inercial
         self.N = N_prev
+        # punto de referencia
         self.O = O_prev
         
+        # se define un marco de referencia que rota respecto a N.y
         self.A = self.N.orientnew(f'A_{name_suffix}', 'Axis', (self.q1, self.N.y))
+        
+        # se define un marco de referecia con respecto a A que rota en los tres ángulos
         self.B = self.A.orientnew(f'B_{name_suffix}', 'Body', (self.q2, self.q1, self.q3), 'XYZ')
         
+        # se representa un vector U a lo largo del eje Bx
         self.U = self.B.x
+        # se representa un vector B a lo largo del eje Bz 
         self.W = self.B.z
 
+        # aqui sabemos que self.d_A es la distancia de N a A donde tenemos un vector AN_
         self.vector_OA = self.d_A * self.N.y
+        # de igual forma con un vector de BA_ en dirección de Ay
         self.vector_AB = self.d_B * self.A.y
+
+        # se ubica un punto A en el sistema coordenado A
         self.A_point = self.O.locatenew(f'A_point_{name_suffix}', self.vector_OA)
+        
+        # se ubica un punto B en el sistema coordenado B
         self.B_point = self.A_point.locatenew(f'B_point_{name_suffix}', self.vector_AB)
+        
         self.coords_B = self.B_point.pos_from(self.O).express(self.N)
         
         self.vector1 = self.coords_B + self.delta_x * self.B.x
@@ -69,7 +84,7 @@ N_inicial = ReferenceFrame('N')
 O_inicial = Point('O')
 
 parametros = [
-    (10, 20, 30, 1, 1, 0.5),  # (q1, q2, q3, d_A, d_B, delta_x) d_A + d_B = L
+    (10, 20, 30, 1, 1, 0.5),  # (q1 (y), q2(z), q3(x), d_A, d_B, delta_x) d_A + d_B = L
     (15, 25, 35, 1, 1, 0.5),
 ]
 
